e8fc71538a8f3fcdc413058ed6a526efdc99ec66
Adrian Stoewer <adrian@stoewer.me>
2020-02-26 20:11:03 +0100

    Function to convert to upper snake case
    Closes #6

--- snake.go
+++ snake.go
@@ -9,7 +9,12 @@ import (
 
 // SnakeCase converts a string into snake case.
 func SnakeCase(s string) string {
-	return lowerDelimiterCase(s, '_')
+	return delimiterCase(s, '_', false)
+}
+
+// UpperSnakeCase converts a string into snake case with capital letters.
+func UpperSnakeCase(s string) string {
+	return delimiterCase(s, '_', true)
 }
 
 // lowerDelimiterCase converts a string into snake_case or kebab-case depending on
@@ -42,6 +47,46 @@ func lowerDelimiterCase(s string, delimiter rune) string {
 			buffer = append(buffer, delimiter)
 		}
 		buffer = append(buffer, toLower(curr))
+	}
+
+	return string(buffer)
+}
+
+// delimiterCase converts a string into snake_case or kebab-case depending on the delimiter passed
+// as second argument. When upperCase is true the result will be UPPER_SNAKE_CASE or UPPER-KEBAB-CASE.
+func delimiterCase(s string, delimiter rune, upperCase bool) string {
+	s = strings.TrimSpace(s)
+	buffer := make([]rune, 0, len(s)+3)
+
+	adjustCase := toLower
+	if upperCase {
+		adjustCase = toUpper
+	}
+
+	var prev rune
+	var curr rune
+	for _, next := range s {
+		if isDelimiter(curr) {
+			if !isDelimiter(prev) {
+				buffer = append(buffer, delimiter)
+			}
+		} else if isUpper(curr) {
+			if isLower(prev) || (isUpper(prev) && isLower(next)) {
+				buffer = append(buffer, delimiter)
+			}
+			buffer = append(buffer, adjustCase(curr))
+		} else if curr != 0 {
+			buffer = append(buffer, adjustCase(curr))
+		}
+		prev = curr
+		curr = next
+	}
+
+	if len(s) > 0 {
+		if isUpper(curr) && isLower(prev) && prev != 0 {
+			buffer = append(buffer, delimiter)
+		}
+		buffer = append(buffer, adjustCase(curr))
 	}
 
 	return string(buffer)
